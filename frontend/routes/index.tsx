4d2fac755d243766fff0f398bf0eb1059c04d995
vansnoden <atagong5@gmail.com>
2024-10-23 15:52:16 +0300

    update

--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -17,9 +17,9 @@ export const handler: Handlers = {
 function Login() {
   return (
     <form method="post" action="/api/login">
-      <input type="text" name="username" />
-      <input type="password" name="password" />
-      <button type="submit">Submit</button>
+      <input className="form-field" type="text" name="username" />
+      <input className="form-field" type="password" name="password" />
+      <button className="btn" type="submit">Submit</button>
     </form>
   );
 }
@@ -31,7 +31,7 @@ export default function Home({ data }: PageProps<Data>) {
       <div>
         You currently {data.isAllowed ? "are" : "are not"} logged in.
       </div>
-      {!data.isAllowed ? <Login /> : <a href="/api/logout">Logout</a>}
+      {!data.isAllowed ? <Login /> : <a className="btn btn-warning" href="/api/logout">Logout</a>}
     </div>
   );
 }
