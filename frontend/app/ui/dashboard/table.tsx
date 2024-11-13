91d28dcc411dbd78b76804f91afc8014d3b9649b
vansnoden <atagong5@gmail.com>
2024-11-13 16:59:18 +0300

    update

--- frontend/app/ui/dashboard/table.tsx
+++ frontend/app/ui/dashboard/table.tsx
@@ -20,10 +20,10 @@ export default async function DataTable({
     <div className="mt-6 flow-root">  
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <table className="hidden min-w-full text-gray-900 md:table">
+          <table className="hidden min-w-full text-gray-900 md:table table-auto">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6 ">
                   Country
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
@@ -52,25 +52,25 @@ export default async function DataTable({
                   key={plant_data.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.country}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.site_name}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.plant_name}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.scientific_name}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.family}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.taxon}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-3">
+                    <td className="px-3 py-3 break-all">
                         {plant_data.kingdom}
                     </td>
                 </tr>
