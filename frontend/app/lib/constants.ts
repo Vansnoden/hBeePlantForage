d6cbd60222fa20e9f0fce8b0dd2977c79a4ac446
vansnoden <atagong5@gmail.com>
2024-11-15 17:20:09 +0300

    update

--- frontend/app/lib/constants.ts
+++ frontend/app/lib/constants.ts
@@ -1,6 +1,6 @@
-export const BASE_API_URL = "http://212.56.40.148:8000"
+export const BASE_API_URL = "http://backend:8000"
 export const AUTH_URL = BASE_API_URL + "/token"
 export const USERINFO_URL = BASE_API_URL + "/users/details/me"
-export const GEOSERVER_BASE_URL = "http://212.56.40.148:3002/geoserver"
+export const GEOSERVER_BASE_URL = "http://geoserver:8080/geoserver"
 export const DASHBOARD_DATA_URL = BASE_API_URL + "/data/dashboard"
 export const PLANT_DATA_URL = BASE_API_URL + "/data/get"
