42e18a8e8b8ae44db5d9ecef95adbc227b3d3f98
salvador <atagong5@gmail.com>
2025-02-27 16:54:23 +0300

    login page updated

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,14 +3,16 @@ import LoginForm from '../ui/login-form';
  
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-yellow-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            Log In
+    <div className="home-bg">
+      <div className="flex items-center justify-center md:h-screen">
+        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+          <div className="flex h-20 w-full items-end rounded-lg bg-yellow-500 p-3 md:h-36">
+            <div className="w-32 text-white md:w-36">
+              Log In
+            </div>
           </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
     </div>
   );
