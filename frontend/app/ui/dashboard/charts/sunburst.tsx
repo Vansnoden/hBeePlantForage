965a5a506642e5481021bf83aa7eaee4675b27a6
salvador <atagong5@gmail.com>
2025-04-08 18:39:14 +0300

    update

--- frontend/app/ui/dashboard/charts/sunburst.tsx
+++ frontend/app/ui/dashboard/charts/sunburst.tsx
@@ -39,12 +39,12 @@ const SunburstChart = (props: { data: any, width:number, height:number , show_la
   
         // Create the arc generator.
         const arc = d3.arc()
-            .startAngle(d => d?.x0)// eslint-disable-line
-            .endAngle(d => d?.x1)// eslint-disable-line
-            .padAngle(d => Math.min((d?.x1 - d?.x0) / 2, 0.005))// eslint-disable-line
+            .startAngle(d => d.x0)// eslint-disable-line
+            .endAngle(d => d.x1)// eslint-disable-line
+            .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))// eslint-disable-line
             .padRadius(radius * 1.5)
-            .innerRadius(d => d?.y0 * radius)// eslint-disable-line
-            .outerRadius(d => Math.max(d?.y0 * radius, d?.y1 * radius - 1))// eslint-disable-line
+            .innerRadius(d => d.y0 * radius)// eslint-disable-line
+            .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))// eslint-disable-line
   
         // Create the SVG container.
         const svg = d3.select(chartCtnRef.current) // eslint-disable-line
@@ -61,16 +61,16 @@ const SunburstChart = (props: { data: any, width:number, height:number , show_la
             .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)// eslint-disable-line
             .attr("pointer-events", d => arcVisible(d.current) ? "auto" : "none")// eslint-disable-line
     
-            .attr("d", d => arc(d.current));// eslint-disable-line
+            .attr("d", d:any => arc(d.current));// eslint-disable-line
   
         // Make them clickable if they have children.
-        path.filter(d => d.children)// eslint-disable-line
+        path.filter(d => d?.children)// eslint-disable-line
             .style("cursor", "pointer")
             .on("click", clicked);
   
         const format = d3.format(",d");
         path.append("title")
-            .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);// eslint-disable-line
+            .text(d => `${d.ancestors().map(d => d?.data.name).reverse().join("/")}\n${format(d?.value)}`);// eslint-disable-line
     
         const label = svg.append("g")
             .attr("pointer-events", "none")
@@ -80,9 +80,9 @@ const SunburstChart = (props: { data: any, width:number, height:number , show_la
         .data(root.descendants().slice(1))
         .join("text")
             .attr("dy", "0.35em")
-            .attr("fill-opacity", d => +labelVisible(d.current))// eslint-disable-line
-            .attr("transform", d => labelTransform(d.current))// eslint-disable-line
-            .text(d => d.data.name);// eslint-disable-line
+            .attr("fill-opacity", d => +labelVisible(d?.current))// eslint-disable-line
+            .attr("transform", d => labelTransform(d?.current))// eslint-disable-line
+            .text(d => d?.data.name);// eslint-disable-line
     
         const parent = svg.append("circle")
             .datum(root)
@@ -92,39 +92,39 @@ const SunburstChart = (props: { data: any, width:number, height:number , show_la
             .on("click", clicked);
     
         // Handle zoom on click.
-        function clicked(event, p) {// eslint-disable-line
-        parent.datum(p.parent || root);
-    
-        root.each(d => d.target = {// eslint-disable-line
-            x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-            x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-            y0: Math.max(0, d.y0 - p.depth),
-            y1: Math.max(0, d.y1 - p.depth)
-        });
-    
-        const t = svg.transition().duration(event.altKey ? 7500 : 750);
-    
-        // Transition the data on all arcs, even the ones that aren’t visible,
-        // so that if this transition is interrupted, entering arcs will start
-        // the next transition from the desired position.
-        path.transition(t)
-            .tween("data", d => {
-                const i = d3.interpolate(d.current, d.target);// eslint-disable-line
-                return t => d.current = i(t);// eslint-disable-line
-            })
-            .filter(function(d) {// eslint-disable-line
-            return + this.getAttribute("fill-opacity") || arcVisible(d.target);// eslint-disable-line
-            })
-            .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)// eslint-disable-line
-            .attr("pointer-events", d => arcVisible(d.target) ? "auto" : "none") // eslint-disable-line
-    
-            .attrTween("d", d => () => arc(d.current));// eslint-disable-line
-    
-        label.filter(function(d) {// eslint-disable-line
-            return +this.getAttribute("fill-opacity") || labelVisible(d.target);// eslint-disable-line
-            }).transition(t)
-            .attr("fill-opacity", d => +labelVisible(d.target))// eslint-disable-line
-            .attrTween("transform", d => () => labelTransform(d.current));// eslint-disable-line
+        function clicked(event: any, p: any) {// eslint-disable-line
+            parent.datum(p.parent || root);
+        
+            root.each(d => d.target = {// eslint-disable-line
+                x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+                y0: Math.max(0, d.y0 - p.depth),
+                y1: Math.max(0, d.y1 - p.depth)
+            });
+        
+            const t = svg.transition().duration(event.altKey ? 7500 : 750);
+        
+            // Transition the data on all arcs, even the ones that aren’t visible,
+            // so that if this transition is interrupted, entering arcs will start
+            // the next transition from the desired position.
+            path.transition(t)
+                .tween("data", d => {// eslint-disable-line
+                    const i = d3.interpolate(d.current, d.target);// eslint-disable-line
+                    return t => d.current = i(t);// eslint-disable-line
+                })
+                .filter(function(d:any) {// eslint-disable-line
+                return + this.getAttribute("fill-opacity") || arcVisible(d.target);// eslint-disable-line
+                })
+                .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)// eslint-disable-line
+                .attr("pointer-events", d => arcVisible(d.target) ? "auto" : "none") // eslint-disable-line
+        
+                .attrTween("d", d => () => arc(d.current));// eslint-disable-line
+        
+            label.filter(function(d:any) {// eslint-disable-line
+                return +this.getAttribute("fill-opacity") || labelVisible(d.target);// eslint-disable-line
+                }).transition(t)
+                .attr("fill-opacity", d => +labelVisible(d.target))// eslint-disable-line
+                .attrTween("transform", d => () => labelTransform(d.current));// eslint-disable-line
         }
         
         function arcVisible(d) {// eslint-disable-line
