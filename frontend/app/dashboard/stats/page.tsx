b86587fb5ae98df9586202ceb310bf7884500bf4
salvador <atagong5@gmail.com>
2025-04-08 22:14:32 +0300

    update

--- frontend/app/dashboard/stats/page.tsx
+++ frontend/app/dashboard/stats/page.tsx
@@ -1,12 +1,13 @@
-import { useSession } from 'next-auth/react';
-// import { getToken } from '../../lib/actions'
+// import { useSession } from 'next-auth/react';
+import { getToken } from '../../lib/actions'
 import StatsComponent from '../../ui/dashboard/statsComponent'
 
 
-export default function StatsPage() {
+export default async function StatsPage() {
     // Server-side: based on HTTP resquest cookie only
-    const { data: session, status } = useSession();
-    // const token = await getToken() as string;
-    console.log(status);
-    return <StatsComponent token={(session?.user as any).accessToken} /> // eslint-disable-line
+    // const { data: session, status } = useSession();
+    const token = await getToken() as string;
+    // console.log(status);
+    // return <StatsComponent token={(session?.user as any).accessToken} /> // eslint-disable-line
+    return <StatsComponent token={token} /> 
 }
