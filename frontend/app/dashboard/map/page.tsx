d844eb7d2b784d07d3ed20237b635b2ba422676f
salvador <atagong5@gmail.com>
2025-04-08 22:04:09 +0300

    update

--- frontend/app/dashboard/map/page.tsx
+++ frontend/app/dashboard/map/page.tsx
@@ -1,10 +1,12 @@
 
-import { getToken } from "@/app/lib/actions";
+// import { getToken } from "@/app/lib/actions";
 import MapComponent from "@/app/ui/dashboard/map";
+import { useSession } from "next-auth/react";
 
-export default async function Map(){
+export default function Map(){
 
-    const token = await getToken() as string;
-    
-    return <MapComponent token={token}/>
+    // const token = await getToken() as string;
+    const { data: session, status } = useSession();
+    console.log(status);
+    return <MapComponent token={(session?.user as any).accessToken}/>  // eslint-disable-line
 }
