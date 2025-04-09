42381b1414c6848988312b04fe765525230d6c3c
salvador <atagong5@gmail.com>
2025-04-09 16:47:42 +0300

    right auth pattern from nextauth 5 bet implemented

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,3 +1,57 @@
+import NextAuth from "next-auth";
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { AUTH_URL, USERINFO_URL } from './app/lib/constants';
+import { cookies } from 'next/headers'
+
+
+async function getUser(token: string) {
+    const userData = await fetch(USERINFO_URL, {
+        method: 'GET',
+        headers: {
+            "Authorization": token
+        }
+    }).then((res) => res.json())
+    return userData;
+}
+
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                .object({ username: z.string(), password: z.string().min(4) })
+                .safeParse(credentials);
+                const formData = new FormData();
+                formData.append("username", parsedCredentials.data?.username as string);
+                formData.append("password", parsedCredentials.data?.password as string);
+                const response = await fetch(AUTH_URL, {
+                    method: 'POST',
+                    body: formData
+                })
+                const res_content = response.json();
+                if ((await res_content).token_type){
+                    // set cookie
+                    const cookieStore = await cookies();
+                    cookieStore.set("auth-token", (await res_content).token_type + "__" + (await res_content).access_token, {
+                        maxAge: 60 * 60 * 24 * 7, // One week
+                        secure: false,
+                        httpOnly: false,
+                        path: "/"
+                    })
+                    const userData = await getUser((await res_content).token_type + " " + (await res_content).access_token)
+                    return userData;
+                }else{
+                    console.log('Invalid credentials');
+                    return null;
+                }
+            },
+        })
+    ],
+})
+
+/*
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
@@ -52,4 +106,4 @@ export const { auth, signIn, signOut } = NextAuth({
   })],
 });
 
-
+*/
