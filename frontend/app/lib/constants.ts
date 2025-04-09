b1f05778ac7d2834817c4f98154b4f7dfb0f40e3
salvador <atagong5@gmail.com>
2025-04-09 09:37:39 +0300

    update

--- frontend/app/lib/constants.ts
+++ frontend/app/lib/constants.ts
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 export const BASE_API_URL = "http://212.56.40.148:8000"
 export const AUTH_URL = BASE_API_URL + "/token"
 export const USERINFO_URL = BASE_API_URL + "/users/details/me"
 export const GEOSERVER_BASE_URL = "http://212.56.40.148:3002/geoserver"
+=======
+export const BASE_API_URL = "https://beehealth.icipe.org/api"
+export const AUTH_URL = BASE_API_URL + "/token"
+export const USERINFO_URL = BASE_API_URL + "/users/details/me"
+export const GEOSERVER_BASE_URL = "https://beehealth.icipe.org/mygeoserver"
+>>>>>>> fc198d4 (update)
 export const DASHBOARD_DATA_URL = BASE_API_URL + "/data/dashboard"
 export const PLANT_DATA_URL = BASE_API_URL + "/data/get"
 export const FAMILY_DATA_URL = BASE_API_URL + "/data/family?fname="
